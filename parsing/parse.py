20aba53900c8a9c193e849a9785d82c4dc0dd47b
jeean0668 <gijeeankyung@naver.com>
2022-06-27 14:47:48 +0900

    finished parsing debug

--- parsing/parse.py
+++ parsing/parse.py
@@ -26,6 +26,7 @@ def parsing_data(excel, debug = False):
         for _, row in excel.iteritems():
             keywords = row.split(',')
             ret.append(keywords)
+        # 불용어를 처리해준다.
         return ret
     
 def split_src_target(news, frame):
@@ -41,30 +42,46 @@ def save_to_excel(frame, path, name):
 
     columns = ['src', 'trg', 'weight']
     df = pd.DataFrame([(k[0], k[1], v) for k, v in frame.items()], columns = columns)
-    df.to_csv(os.path.join(path, name), index = False)
+    try:
+        df.to_csv(os.path.join(path, name), index = False)
+    except:
+        print(f"Error arise while saving to excel {name}...")
     
 if __name__ == "__main__":
     
     root = 'parsing/news/Islam'
     religion = 'Islam'
     debuggued = True
-    frame = defaultdict(int)
+    
     path = "parsing/news/Islam_parsed"
     
     file_names = os.listdir(root)
     
-    data = excel_open(file_names[0], root, debug = False)
-    # using only keyword column
-    parsed = parsing_data(data, debug = False)
-    
-    if debuggued:
-        split_src_target(parsed[:10], frame)
-    else:
-        split_src_target(parsed, frame)
+    for file_name in file_names:
         
-    if debuggued:
-        save_to_excel(frame, path, file_names[0] + '_debug')
-    else:
-        save_to_excel(frame, path, file_names[0])
+        frame = defaultdict(int)
+        
+        data = excel_open(file_name, root, debug = False)
+        
+        # using only keyword column
+        print(f"parsing the {file_name}...")
+        parsed = parsing_data(data, debug = False)
+        print(f"{file_name} parsing finished...")
+        
+        print(f"spliting the {file_name}...")
+        if debuggued:
+            split_src_target(parsed[:10], frame)
+        else:
+            split_src_target(parsed, frame)
+        print(f"{file_name} spliting finished...")
+        
+        print(f"saving the {file_name}...")
+        if debuggued:
+            save_to_excel(frame, path, file_name + '_debug')
+        else:
+            save_to_excel(frame, path, file_name)
+        print(f"{file_name} saving finished...")
+        print(f"{file_name} finished!!!")
+        print()
    
     
